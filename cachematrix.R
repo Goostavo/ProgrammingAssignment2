5010e42dbfde3f9d59173cf2b4da5f97a4638589
Gustavo Garcia <goostavobg@gmail.com>
2014-07-27 17:34:42 -0300

    Implementantion of the makeCacheMatrix; Header comments writeen

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## CacheMatrix
+## This file contains the objects and functions related to the Cache Matrix
+## The CacheMatrix is a matrix that can cache it's inverse
 
-## Write a short comment describing this function
 
+## makeCacheMatrix is the constructor for the cacheMatrix object
+# args:
+# x -> Contains the initial value for the CacheMatrix
+#
+# Returns:
+# The Cache Matrix object
 makeCacheMatrix <- function(x = matrix()) {
-
+    inverseX <- NULL
+    #setters and getters
+    set <- function(newValue){
+        x <<- newValue
+        inverseX <<- NULL
+    }
+    get <- function() x
+    
+    # Setter and getter for the CacheMatrix Inverse value
+    # note: The setInverse does NOT compute the inverse!
+    setInverse <- function(solve) inverseX <<- solve
+    getInverse <- function() inverseX
+    
+    # functions list
+    list(set = set, get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
 }
 
 
